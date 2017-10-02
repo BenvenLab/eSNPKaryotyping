fe3f6d6daf4ccf7d90269a158c5175fe3a96409f
BenvenLab <uriwsbn@gmail.com>
2017-10-02 15:36:11 +0300

    Update CreateVCF.R

--- eSNPKaryotyping/R/CreateVCF.R
+++ eSNPKaryotyping/R/CreateVCF.R
@@ -10,36 +10,34 @@
 
 
 CreateVCF<-function(Directory,Genome_Fa,Picard_Path,GATK_Path){
-  newDir = Directory
-  Genome_Fa=
-  
+
   setwd(newDir)
-  comm = paste("java -jar ",Picard_Path, "picard.jar AddOrReplaceReadGroups.jar  I=accepted_hits.bam O=accepted_hits_rg.bam ID=\"n\" LB=\"lb\" PL=\"illumina\" PU=\"pu\" SM=\"ES\"",sep="")
+  comm = paste("java -jar ",Picard_Path, "picard.jar AddOrReplaceReadGroups  I=",Directory,"accepted_hits.bam O=accepted_hits_rg.bam ID=\"n\" LB=\"lb\" PL=\"illumina\" PU=\"pu\" SM=\"ES\"",sep="")
   print("======================")
   print(comm)
   print("======================")
   system(comm)
   setwd("~/")
   
-  comm = paste("java -jar ",Picard_Path, "picard.jar ReorderSam.jar I=",newDir, "accepted_hits_rg.bam O=",newDir, "accepted_hits_rg_sorted.bam R=",Genome_Fa, sep="")
+  comm = paste("java -jar ",Picard_Path, "picard.jar ReorderSam I=",Directory, "accepted_hits_rg.bam O=",newDir, "accepted_hits_rg_sorted.bam R=",Genome_Fa, sep="")
   print("======================")
   print(comm)
   print("======================")
   system(comm)
   
-  comm = paste("java -jar ",Picard_Path, "picard.jar BuildBamIndex.jar I=",newDir, "accepted_hits_rg_sorted.bam", sep="")
+  comm = paste("java -jar ",Picard_Path, "picard.jar BuildBamIndex I=",Directory, "accepted_hits_rg_sorted.bam", sep="")
   print("======================")
   print(comm)
   print("======================")
   system(comm)
   
-  comm = paste("java -jar ",GATK_Path, "GenomeAnalysisTK.jar -T SplitNCigarReads -R ",Genome_Fa," -I ",newDir, "accepted_hits_rg_sorted.bam -o ",newDir, "split.bam -rf ReassignOneMappingQuality -RMQF 255 -RMQT 60 -U ALLOW_N_CIGAR_READS", sep="") 
+  comm = paste("java -jar ",GATK_Path, "GenomeAnalysisTK.jar -T SplitNCigarReads -R ",Genome_Fa," -I ",Directory, "accepted_hits_rg_sorted.bam -o ",Directory, "split.bam -rf ReassignOneMappingQuality -RMQF 255 -RMQT 60 -U ALLOW_N_CIGAR_READS", sep="") 
   altComm = paste(comm,"--fix_misencoded_quality_scores")
   print("======================")
   print(comm)
   print("======================")
   system(comm)
-  if (!file.exists( paste(newDir, "split.bam",sep=""))) {
+  if (!file.exists( paste(Directory, "split.bam",sep=""))) {
     print("======================")
     print("Alternative Command")
     print(altComm)
@@ -49,7 +47,7 @@ CreateVCF<-function(Directory,Genome_Fa,Picard_Path,GATK_Path){
   }
   
   
-  comm = paste("java -jar ",GATK_Path, "GenomeAnalysisTK.jar -T HaplotypeCaller -R ",Genome_Fa," -I ",newDir, "split.bam -recoverDanglingHeads -dontUseSoftClippedBases -stand_call_conf 20.0 -stand_emit_conf 20.0 -o ",newDir, "variants_output.vcf", sep="")
+  comm = paste("java -jar ",GATK_Path, "GenomeAnalysisTK.jar -T HaplotypeCaller -R ",Genome_Fa," -I ",Directory, "split.bam -dontUseSoftClippedBases -stand_call_conf 10.0 -o ",Directory, "variants_output.vcf", sep="")
   print("======================")
   print(comm)
   print("======================")
