55a85586d7b7cd33dab2ffeaa521ea836fb8b837
BenvenLab <uriwsbn@gmail.com>
2017-10-02 12:31:44 +0300

    Update and rename CreatVCF.R to CreateVCF.R

--- eSNPKaryotyping/R/CreateVCF.R
+++ eSNPKaryotyping/R/CreateVCF.R
@@ -1,4 +1,4 @@
-#' CreatVCF
+#' CreateVCF
 #'
 #' Create the VCF (Variant Call Format) file 
 #' @param Directory The Path of to the BAM Directory
@@ -9,25 +9,25 @@
 #' @return None
 
 
-CreatVCF<-function(Directory,Genome_Fa,Picard_Path,GATK_Path){
+CreateVCF<-function(Directory,Genome_Fa,Picard_Path,GATK_Path){
   newDir = substring(Directory,3)
   Genome_Fa=substring(Genome_Fa,3)
   
   setwd(paste( "~/",newDir, sep=""))
-  comm = paste("java -jar ",Picard_Path, "AddOrReplaceReadGroups.jar  I=accepted_hits.bam O=accepted_hits_rg.bam ID=\"n\" LB=\"lb\" PL=\"illumina\" PU=\"pu\" SM=\"ES\"",sep="")
+  comm = paste("java -jar ",Picard_Path, "picard.jar AddOrReplaceReadGroups.jar  I=accepted_hits.bam O=accepted_hits_rg.bam ID=\"n\" LB=\"lb\" PL=\"illumina\" PU=\"pu\" SM=\"ES\"",sep="")
   print("======================")
   print(comm)
   print("======================")
   system(comm)
   setwd("~/")
   
-  comm = paste("java -jar ",Picard_Path, "ReorderSam.jar I=",newDir, "accepted_hits_rg.bam O=",newDir, "accepted_hits_rg_sorted.bam R=",Genome_Fa, sep="")
+  comm = paste("java -jar ",Picard_Path, "picard.jar ReorderSam.jar I=",newDir, "accepted_hits_rg.bam O=",newDir, "accepted_hits_rg_sorted.bam R=",Genome_Fa, sep="")
   print("======================")
   print(comm)
   print("======================")
   system(comm)
   
-  comm = paste("java -jar ",Picard_Path, "BuildBamIndex.jar I=",newDir, "accepted_hits_rg_sorted.bam", sep="")
+  comm = paste("java -jar ",Picard_Path, "picard.jar BuildBamIndex.jar I=",newDir, "accepted_hits_rg_sorted.bam", sep="")
   print("======================")
   print(comm)
   print("======================")
